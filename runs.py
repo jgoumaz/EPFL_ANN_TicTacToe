f6cc5877b0a101f5852504e97669a83b56e653ff
jgoumaz <56827471+jgoumaz@users.noreply.github.com>
2022-06-04 14:31:14 +0200

    minor update

--- runs.py
+++ runs.py
@@ -11,7 +11,7 @@ def compute_M_rand(Player):
     pass
 
 
-def run_against_Opt(Player, n_games=100):
+def run_against_Opt(Player, n_games=100, opt_eps=0.2, return_M_opt=False, return_M_rand=False):
     rewards = []
     average_rewards = []
     M_opts = []
@@ -20,17 +20,17 @@ def run_against_Opt(Player, n_games=100):
     ties = 0
     losses = 0
     Tictactoe = TictactoeEnv()
-    Opt = OptimalPlayer()
+    Opt = OptimalPlayer(epsilon=opt_eps)
     for n in range(n_games):
         Tictactoe.reset()
         Opt.set_player(n)
         Player.set_player(n+1)
         for round in range(9):
-            if Tictactoe.get_current_player() == Opt.player:
-                move = Opt.act()
-            else:
-                move = Player.act()
             old_grid, _, _ = Tictactoe.observe()
+            if Tictactoe.get_current_player() == Opt.player:
+                move = Opt.act(old_grid)
+            else:
+                move = Player.act(old_grid)
             new_grid, end, winner = Tictactoe.step(move)
             if Tictactoe.get_current_player() == Player.player:
                 reward = Tictactoe.reward(player=Player.player)
@@ -47,7 +47,7 @@ def run_against_Opt(Player, n_games=100):
         if n%250 == 0:
             average_rewards.append(sum(rewards)/len(rewards))
             rewards = []
-            M_opts.append(compute_M_opt(Player))
-            M_rands.append(compute_M_rand(Player))
+            if return_M_opt: M_opts.append(compute_M_opt(Player))
+            if return_M_rand: M_rands.append(compute_M_rand(Player))
     return average_rewards, M_opts, M_rands
 
